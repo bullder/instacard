671f04d212da59947d9b5f04bcff09a6dee055c6
Michael Garifullin <michael.garifullin@smartbox.com>
2023-07-28 00:30:09 +0100

    retrive acc

--- server/get/main.go
+++ server/get/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"github.com/Davincible/goinsta/v3"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"os"
 )
 
 type Response struct {
 	StatusCode int         `json:"statusCode"`
 	Body       interface{} `json:"body"`
+}
+
+type Profile struct {
+	Name   string `json:"name"`
+	Bio    string `json:"bio"`
+	Images []Img `json:"images"`
+}
+
+type Img struct {
+	Img  string `json:"img"`
+	Caption string `json:"caption"`
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
@@ -21,11 +35,7 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Respon
 		}, nil
 	}
 
-	responseData := map[string]interface{}{
-		"message": "Hello, this is a JSON response from AWS Lambda!" + name,
-	}
-
-	jsonData, err := json.Marshal(responseData)
+	jsonData, err := json.Marshal(get(name))
 	if err != nil {
 		return Response{}, err
 	}
@@ -34,6 +44,35 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (Respon
 		StatusCode: 200,
 		Body:       string(jsonData),
 	}, nil
+}
+
+func get(name string) Profile {
+	insta := goinsta.New(os.Getenv("user"), os.Getenv("password"))
+	if err := insta.Login(); err != nil {
+		panic(err)
+	}
+
+	profile, err := insta.VisitProfile(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := profile.User
+	feed := profile.Feed
+
+	images := make([]Img, len(feed.Items))
+	for i, image := range feed.Items {
+		images[i] = Img{
+			Img: image.Images.GetBest(),
+			Caption: image.Caption.Text,
+		}
+	}
+
+	return Profile{
+		Name:   name,
+		Bio:    user.Biography,
+		Images: images,
+	}
 }
 
 func main() {
