f5c6bb979bf20fbd1ea737657528f9b2970cd4b2
Michael Garifullin <michael.garifullin@smartbox.com>
2023-07-27 19:13:59 +0100

    create another response

--- server/get/main.go
+++ server/get/main.go
@@ -6,13 +6,22 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyResponse struct {
-	Message string `json:"Answer:"`
+type Response struct {
+	StatusCode int               `json:"statusCode"`
+	Headers    map[string]string `json:"headers"`
+	Body       string            `json:"body"`
 }
 
-func HandleRequest(ctx context.Context) (MyResponse, error) {
-	//return fmt.Sprintf("Hello %s!", name.Name ), nil
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", "Mike", 38)}, nil
+func HandleRequest(ctx context.Context) (Response, error) {
+	msg := fmt.Sprintf("%s is %d years old!", "Mike", 38)
+	fmt.Println(msg)
+
+	return Response{
+			StatusCode: 1,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+			Body:       msg,
+		},
+		nil
 }
 
 func main() {
